86c8a49b27f9836708a2cdd3f12da269c114bcd1
Tony Barrett <tonyb@gmx.com>
2024-09-10 14:27:00 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+
+export class AppComponent {
+  isActive: boolean = true;
+
+  public toggleActive(): void{
+    this.isActive = !this.isActive;
+  }
+
+}
